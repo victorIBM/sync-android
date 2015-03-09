95c029ba931bf126a5438313ebbdd6cea0a75529
Esteban Laver <emlaver@us.ibm.com>
2015-03-09 10:16:07 -0400

    Add SQLCipher option for test cases

--- sync-core/src/test/java/com/cloudant/sync/datastore/DatastoreTestBase.java
+++ sync-core/src/test/java/com/cloudant/sync/datastore/DatastoreTestBase.java
@@ -25,12 +25,6 @@ import org.junit.Before;
  */
 public abstract class DatastoreTestBase {
 
-    //System property for testing with SQLCipher-based SQLite database for Android
-    //public static final Boolean SQL_CIPHER_ENABLED = Boolean.valueOf(
-    //        System.getProperty("test.sqlcipher.passphrase",Boolean.FALSE.toString()));
-
-    public static final String SQL_CIPHER_ENABLED = System.getProperty("test.sqlcipher.passphrase");
-
     String datastore_manager_dir;
     DatastoreManager datastoreManager;
     BasicDatastore datastore = null;
@@ -40,9 +34,9 @@ public abstract class DatastoreTestBase {
         datastore_manager_dir = TestUtils.createTempTestingDir(this.getClass().getName());
         datastoreManager = new DatastoreManager(this.datastore_manager_dir);
 
-        if(SQL_CIPHER_ENABLED != null) {
-            //Database name with SQLCipher enabled.  Need to have different database names if encryption is enabled.
-            datastore = (BasicDatastore) (this.datastoreManager.openDatastore(getClass().getSimpleName(), "SQLCipherTest"));
+        //Open SQLCipher-based datastore if SQLCipher parameter is 'true'
+        if(Boolean.valueOf(System.getProperty("test.sqlcipher.passphrase"))) {
+            datastore = (BasicDatastore) (this.datastoreManager.openDatastore(getClass().getSimpleName(), new HelperKeyProvider()));
         } else {
             datastore = (BasicDatastore) (this.datastoreManager.openDatastore(getClass().getSimpleName()));
         }
